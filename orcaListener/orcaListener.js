fd28fbf1bd428ea049f39e34cf8685a660ef5bcf
ImOutOfIdeas <64992347+ImOutOfIdeas@users.noreply.github.com>
2022-12-05 13:10:53 -0500

    Added orca project

---
+++ orcaListener/orcaListener.js
@@ -0,0 +1,28 @@
+const IP_ADDR = '128.146.165.227';
+const UDP_PORT = 49161;
+
+const dgram = require('dgram');
+const udpserver = dgram.createSocket('udp4');
+
+var name = " ";
+
+// Error
+udpserver.on('error', (err) => {
+    console.log(`UDP server:\n${err.stack}`);
+    udpserver.close();
+});
+
+// Message
+udpserver.on('message', (msg, rinfo) => {
+    if (name != msg) {
+        name = msg;
+        console.log(`Hello ${name}. How's it going?`);
+    }
+});
+
+udpserver.bind(UDP_PORT);
+
+console.log(`Started Listener!\n\nIP: ${IP_ADDR}\nPort: ${UDP_PORT}\n`);
+
+// Send a run message to orca
+udpserver.send('run', 0, 3, 49160, IP_ADDR);
